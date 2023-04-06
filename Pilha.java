d8aecef076f5f35d832e30baef84c094b8fa7a32
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-04-06 14:57:58 -0400

    e lá vamos nós

--- Pilha.java
+++ Pilha.java
@@ -1,29 +1,54 @@
 public class Pilha {
-    
+
     public Object[] pilha;
     public int posicaoPilha;
 
-
     public Pilha() {
         this.posicaoPilha = -1;
-        this.pilha = new Object[50];
+// indica que esta nula, vazia, pois posição um indica que contém informação
+        this.pilha = new Object[200];
+    }
+
+    public boolean pilhaVazia() {
+        //isEmpty
+        if (this.posicaoPilha == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public int tamanho() {
+        if (this.pilhaVazia()) {
+            return 0;
+        }
+        return this.posicaoPilha + 1;
     }
 
 
+    public Object desempilhar() {
+        //pop
+        if (pilhaVazia()) {
+            return null;
+        }
+        return this.pilha[this.posicaoPilha--];
+    }
+
     public void empilhar(Object valor) {
-        if( this.posicaoPilha < this.pilha.length - 1){
+        // push
+        if (this.posicaoPilha < this.pilha.length - 1) {
             this.pilha[++posicaoPilha] = valor;
         }
-    } 
+    }
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Pilha p = new Pilha();
-        p.empilhar("Sorvete de morango");
-        p.empilhar("Sorvete de choolate");
-        p.empilhar("Sorvete de avelã");
-        p.empilhar("Sorvete de baunilha");
-        p.empilhar(10);
-        System.out.println(p);
-}
-
+        p.empilhar("Dado 1 ");
+        p.empilhar("Dado 2 ");
+        p.empilhar("Dado 3 ");
+        p.empilhar("Dado 4 ");
+        p.empilhar(9);
+                while (p.pilhaVazia() == false) {
+            System.out.println(p.desempilhar());
+        }
+    }
 }
--- index.java
+++
@@ -1,5 +0,0 @@
-//Utilizando a ideia apresentada na aula, os slides que estão disponibilizados e tendo como base a implementação de pilha utilizando Arrays, agora você 
-//irá realizar uma implementação de uma Pilha utilizando Referências. 
-//Esta atividade terá duração de duas semanas para desenvolviment
-//Implemente a estrutura de dados Pilha, com pelo menos as operações push, pop. De tal forma 
-//que os valores sejam armazenados como referências encadeadas, como demonstrado na aula.
